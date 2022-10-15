d00d25d1d7d440213bec0e34613a3ae4d4e8cc18
rico wu <jasonwu@outlook.com>
2022-10-15 16:28:01 +0800

    20221015

--- app/src/main/java/com/example/mydemoservice/MainActivity.java
+++ app/src/main/java/com/example/mydemoservice/MainActivity.java
@@ -1,79 +1,73 @@
 package com.example.mydemoservice;
-
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Button connect;
-    private Button btnalert;
+    private MeyDemoWebSocketClient client;
+    private  SharedPreferences sp;
+    private Intent intent;
+    private  EditText host;
+    private  EditText port;
+    private  CheckBox is_remember;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        host =findViewById(R.id.host);
+        port =findViewById(R.id.port);
+        is_remember = findViewById(R.id.remember);
+        connect = findViewById(R.id.connect);
+        sp = getSharedPreferences("uri",MODE_PRIVATE);
+        intent = new Intent();
+        intent.setClass(MainActivity.this,MyDemoShowLogActivity.class);
+        String connect_host =  sp.getString("connect_host",null);
+        String connect_port =  sp.getString("connect_port",null);
+        host.setText(connect_host);
+        port.setText(connect_port);
 
-        final EditText host = findViewById(R.id.host); //变量不可以在被更改
-        final EditText port = findViewById(R.id.port);
-        final CheckBox is_remember = findViewById(R.id.remember);
-        connect = findViewById(R.id.btn2); //btn2 与 button id为btn2属性关联了
-        btnalert = findViewById(R.id.btn_alert);
+    }
 
-        //绑定listview
-        //ListView lv_demo = findViewById(R.id.show_log_view);
-        //定义数据源
-        //List<String> names = new ArrayList<>();
-        //names.add("a");
-        //names.add("b");
-        //names.add("c");
-        //单元格布局
-        //SimpleAdapter simpleAdapter = new SimpleAdapter();
 
+    @Override
+    protected void onStart() {
+        super.onStart();
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("MainActivity",host.getText().toString());
-                Log.i("MainActivity",port.getText().toString());
-                Log.i("MainActivity", String.valueOf(is_remember.isChecked()));
-                Toast.makeText(MainActivity.this,"host or port not correct",Toast.LENGTH_SHORT).show();
+                URI connect_uri = URI.create("ws://"+host.getText().toString()+":"+port.getText().toString());
+                if(is_remember.isChecked()){
+
+                    SharedPreferences.Editor edit = sp.edit();
+                    edit.putString("connect_host",host.getText().toString());
+                    edit.putString("connect_port",port.getText().toString());
+                    edit.commit();
+                    Toast.makeText(MainActivity.this,"remember connect info",Toast.LENGTH_SHORT).show();
+                }
+                startActivityForResult(intent,1);
             }
         });
+    }
 
-        btnalert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                builder.setTitle("使用对话框").setMessage("设置信息").setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.i("MainActivity","ok");
-                    }
-                }).setNegativeButton("no", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.i("MainActivity","no");
-                    }
-                });
-                AlertDialog ad = builder.create();
-                ad.show();
-            }
-        });
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
